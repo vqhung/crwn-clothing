b9b9ac4e4daa5e58e240d032cb84b5591bec8ddc
Vo <vo@Vos-iMac.local>
2019-07-19 08:21:28 +1000

    update our menu-items to navigate withRouter HOC

--- src/App.js
+++ src/App.js
@@ -1,16 +1,24 @@
-import React, { Component } from 'react';
+import React from 'react';
+import {Route, Switch} from 'react-router-dom'
 import './App.css';
 
 import HomePage from './pages/homepage/homepage.component'
 
-class App extends Component {
-  render() {
-    return (
-      <div className='App'>
-        <HomePage />
-      </div>
-    );
-  }
+const HatsPage = () => (
+  <div>
+    <h1>HATS PAGE</h1>
+  </div>
+)
+
+function App () {
+  return (
+    <div>
+      <Switch>
+        <Route exact path='/' component={HomePage} />
+        <Route exact path='/hats' component={HatsPage} />
+      </Switch>
+    </div>
+  )
 }
 
 export default App;
--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -47,12 +47,10 @@ class Directory extends React.Component {
     render() {
         return (
             <div className='directory-menu'>
-                {this.state.sections.map(({id, imageUrl, title, size}) => (
+                {this.state.sections.map(({id, ...otherSectionProps}) => (
                     <MenuItem
                     key = {id}
-                    title = {title}
-                    imageUrl = {imageUrl}
-                    size = {size}
+                    {...otherSectionProps}
                      />
                 ))}
             </div>
